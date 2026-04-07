0c3c0e8cc6cd3ac9f8971fecf5f21007b1102f74
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-07 13:04:39 +0000

    Add Memos controller

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,13 @@
+class MemosController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+end
---
+++ test/controllers/memos_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class MemosControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get memos_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get memos_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get memos_destroy_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get memos_new_url
+    assert_response :success
+  end
+end
