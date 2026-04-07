e3ba6db27fae047fb00a0df46b8443105a8d0e25
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-07 13:25:42 +0000

    Add Memos controller actions

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,13 +1,20 @@
 class MemosController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def destroy
+    @memos = Memo.all
   end
 
   def new
+    @memo = Memo.new
   end
+
+  def create
+    Memo.create(text: params[:text])
+    redirect_to root_path
+  end
+
+  def destroy
+    Memo.find(params[:id]).destroy
+    #redirect_to root_path
+  end
+
 end
