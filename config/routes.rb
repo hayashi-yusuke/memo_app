a67746cd4e08dfa102b06b60daf00fbed0245164
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-07 13:51:03 +0000

    add view index,new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get "memos/index"
-  get "memos/create"
-  get "memos/destroy"
-  get "memos/new"
+
+  root "memos/index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :memos, only: [:index, :create, :destroy]
+  resources :memos, only: [:index, :new, :create, :destroy]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
