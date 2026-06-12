48d710f37b9139d508e2cca563feb4acf1447b25
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-06-12 15:13:30 +0000

    Add like button to memos index view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
   def create
     @memo = Memo.find(params[:memo_id])
     @memo.likes.create(user: Current.user)
-    redirect_to @memo
+    redirect_to memos_path
   end
 
   def destroy
     @memo = Memo.find(params[:memo_id])
     like = @memo.likes.find_by(user: Current.user)
     like.destroy
-    redirect_to @memo
+    redirect_to memos_path
   end
 end
